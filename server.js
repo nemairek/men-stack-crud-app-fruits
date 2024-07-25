9955c35acea420a5851659bcab8b16a72e6c29c8
Knemaire <kannemaire@gmail.com>
2024-07-25 14:58:53 -0400

    Instal

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const mongoose = require('mongoose')
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+
+const Fruit = require('./models/fruit.js')
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
@@ -9,3 +22,4 @@ app.listen(3000, () => {
 app.get('/', async (req,res) => {
     res.render('index.ejs');
 });
+
